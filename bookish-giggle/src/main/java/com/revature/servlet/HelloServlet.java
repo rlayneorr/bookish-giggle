d6949217ab68140b9427384d6ba789cef44dd271
Ricky Orr <rlayneorr@gmail.com>
2019-06-05 09:38:51 -0400

    changed

--- bookish-giggle/src/main/java/com/revature/servlet/HelloServlet.java
+++ bookish-giggle/src/main/java/com/revature/servlet/HelloServlet.java
@@ -11,6 +11,6 @@ public class HelloServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
 		throws IOException, ServletException {
-		resp.getWriter().write("Hello from my app.");
+		resp.getWriter().write("Hello from my app. This is a change to my app.");
 	}
 }
